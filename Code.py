b10d1bb04dcb1de7160f097b260a418ab8c142cd
neelam-pharswan <neelampharswan2000@gmail.com>
2024-11-05 20:12:33 +0530

    Addes the code

---
+++ Code.py
@@ -0,0 +1,17 @@
+print("\nWelcome to Animals Name Guessing Game:)\nAvailable names are as follows: \n")
+
+import random
+Animals = ["Cow", "Cat", "Dog", "Monkey","Donkey"]
+print(Animals)
+words = random.choice(Animals)
+print(words) # We can remove this to hide from the user
+
+for i in range(3):
+    Animal = input("Enter your guess from these names: ").strip()  
+
+    if Animal.lower() ==words.lower():
+        print("Heyyy You Won:) , You are a Genius!")
+    else:
+        print("You lost!!, Try Again:)") 
+print("Game Over!!!")        
+   
